be4ea7ba19b132c6591d178c340e1424043d2bfd
breczus <breczus@gmail.com>
2020-08-08 21:53:19 +0200

    add gitignore, procfile, udpate ports

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.post("/", function(req, res) {
   if (listName === "Today"){
     item.save();
     res.redirect("/");
-    
+
   } else{
     List.findOne({name:listName}, function(err, foundList){
       foundList.items.push(item);
@@ -144,7 +144,11 @@ app.get('/:customListName', function(req, res) {
 app.get("/about", function(req, res) {
   res.render("about");
 });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
